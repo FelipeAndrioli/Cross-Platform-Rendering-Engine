d04c116c283ab69435119069a2b64dfe7bb71f8d
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-08-23 22:27:52 -0300

    Adding UI as a global component

--- include/global.h
+++ include/global.h
@@ -3,11 +3,13 @@
 #include <iostream>
 
 #include "./window.h"
+#include "./ui.h"
 
 class Global {
     public:
         Global();
         Window m_window;
+        UI m_UI;
 
         void resizeWindow(int new_width, int new_height);
 };
