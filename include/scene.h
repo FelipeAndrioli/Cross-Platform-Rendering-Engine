6ac3447e2696f43a3a534c0377c8cea2037d1816
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-09-09 18:23:13 -0300

    Started to enhance the scene class

--- include/scene.h
+++ include/scene.h
@@ -4,12 +4,21 @@
 #include <vector>
 
 #include "../include/shader.h"
+#include "../include/model.h"
 #include "../src/scenes/pixel_rendering.h"
+
+enum RenderingType {
+    PIXEL,
+    SCENE
+};
 
 class Scene {
     public:
-        Scene();
+        Scene(RenderingType rendering_type);
         ~Scene();
+        std::vector<Model> models;
+
+        // these need to be removed
         Shader *SceneShader;
         std::vector<float> vertices;
         std::vector<unsigned int> indices;
