efef0aad47f3a6bb37ca9e04e6cfabf5958b3d30
Felipe Andrioli <felipeandrioli56@hotmail.com>
2022-10-07 16:00:14 -0300

    Removing unused variables

--- src/state.h
+++ src/state.h
@@ -21,10 +21,6 @@ class State {
         double fps;
         double ms;
         int frames;
-
-        const int TICKS_PER_SECOND = 30;
-        const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
-        const int MAX_FRAMESKIP = 10;
         // End timing
 
         bool config_mode;
