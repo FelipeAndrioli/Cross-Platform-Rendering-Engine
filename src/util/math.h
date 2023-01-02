2d85effe6c8b017ffc56b27434cff3ab3e1ee68e
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-01-02 10:09:50 -0300

    Added struct math file for the only purposing of help the obj loader

---
+++ src/util/math.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace math {
+    typedef struct {
+        float x;
+        float y;
+    } vec2;
+
+    typedef struct {
+        float x;
+        float y;
+        float z;
+    } vec3;
+}
+
